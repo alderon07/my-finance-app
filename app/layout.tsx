f41c84cc48e8fc5d0a8d6d2efb119e7d9671761f
Naqi <naqihaider07@gmail.com>
2024-02-26 22:03:48 -0500

    added layouts and navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 // By adding Inter to the <body> element, the font will be applied throughout your application. Here, you're also adding the Tailwind antialiased class which smooths out the font.
 
+// Any UI you add to the root layout will be shared across all pages in your application. This is a great place to add global styles, fonts, or components that you want to be consistent across your application.
+
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
  
